6db12a284459bf5976b300b18f10c8db7ecd64da
KseniiaHondak <kseniia.khondak@nure.ua>
2017-10-06 19:22:55 +0300

    Initial commit for usermanagement

---
+++ usermanagement/src/main/java/ua/nure/kn156/hondak/User.java
@@ -0,0 +1,61 @@
+package ua.nure.kn156.hondak;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class User {
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private Date date;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getFullName() {
+
+		if (getLastName() == null || getFirstName() == null){
+			throw new IllegalArgumentException("firstName or lastName is NULL");
+		}
+		return new StringBuilder(getLastName()).append(",").append(getFirstName()).toString();
+	}
+
+	public long getAge() {
+		Calendar calendar = Calendar.getInstance();
+		long currentYear = calendar.get(Calendar.YEAR);
+		calendar.setTime(getDate());
+		long yearOfBirth = calendar.get(Calendar.YEAR);
+
+		return currentYear - yearOfBirth;
+	}
+
+}
---
+++ usermanagement/src/test/java/ua/nure/kn156/hondak/UserTest.java
@@ -0,0 +1,58 @@
+package ua.nure.kn156.hondak;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+	private static final int AGE = 19;
+	private User user;
+	private Date date;
+
+	@Override
+	protected void setUp() throws Exception {
+
+		super.setUp();
+		user = new User();
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(1998, Calendar.JANUARY, 28);
+		date = calendar.getTime();
+	}
+
+	public void testGetFullName() {
+		user.setFirstName("Kseniia");
+		user.setLastName("Hondak");
+		assertEquals("Hondak,Kseniia", user.getFullName());
+	}
+
+	public void testGetFullLastNameIsNull() {
+		user.setLastName("Hondak");
+		try {
+			user.getFullName();
+			fail("Should have thrown an exception");
+
+		} catch (IllegalArgumentException e) {
+			System.out.println("lastName is NULL");
+		}
+
+	}
+
+	public void testGetFullFirstNameIsNull() {
+		user.setFirstName("Kseniia");
+		try {
+			user.getFullName();
+			fail("Should have thrown an exception");
+
+		} catch (IllegalArgumentException e) {
+			System.out.println("firstName is NULL");
+		}
+
+	}
+
+	public void testGetAge() {
+		user.setDate(date);
+		assertEquals(AGE, user.getAge());
+	}
+
+}
